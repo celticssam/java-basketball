43a06ff026f51e52a5c90224fa7b2de20b771fa7
celticssam <74743702+celticssam@users.noreply.github.com>
2021-04-22 18:07:28 -0400

    Update game.java

--- game.java
+++ game.java
@@ -10,4 +10,18 @@
 public class Game extends JFrame{
 
 private Image hoop, basketballIcon;
-private GamePanel
+private GamePanel game;
+  
+  
+  public Game(){
+    super("basketball game");
+    hoop = new ImageIcon("Hoop.gif").getImage();
+    BasketballIcon = new ImageIcon("basketballIcon.png").getImage();
+    game = new GamePanel(BasketballIcon, hoop);
+    initialize();
+  }
+  
+  private void initialize(){
+    
+    setSize(500,500);
+    setLocationRelativeTo(null);
