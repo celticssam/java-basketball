98929bfea25db65825efa79db4fac203c8e1b6a2
celticssam <74743702+celticssam@users.noreply.github.com>
2021-05-03 18:00:57 -0400

    Update GamePanel.java

--- GamePanel.java
+++ GamePanel.java
@@ -11,3 +11,19 @@ import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 import javax.swing.JPanel;
+
+
+public class GamePanel extends JPanel implements KeyListener{
+  //fields
+  private int x,i;
+  private Image ball, hoop;
+  private int velx, vely;
+  private int ballX, ballY;
+  private int lives, makes, misses;
+  private boolean MOVING, ENDOFGAME, RESTART;
+  
+  
+  
+  
+  public GamePanel (Image ball, Image hoop){
+  
